d7ef4018fc7e9e7d5e2dc7eebbc7b1c05ce4ffe6
Jerin Prakash <jerinprakash892@gmail.com>
2026-02-12 11:12:44 +0530

    code added

--- src/StudentClass.java
+++ src/StudentClass.java
@@ -2,10 +2,24 @@ import java.util.Scanner;
 
 class Student {
    
+	String name;
+	int rollno;
 
     // TODO: Create a parameterized constructor Student(String name, int rollNo)
-    
+
+	Student(String name, int rollno){
+
+		this.name=name;
+		this.rollno=rollno;
+
+	}    
     // TODO: Create a method or use direct access to print details in the required format
+
+	void print(){
+
+		System.out.println("Name: "+name+" Roll: "+rollno);
+
+	}
 }
 
 public class StudentClass {
@@ -21,5 +35,27 @@ public class StudentClass {
         // TODO: Create two Student objects
         
         // TODO: Print details for both students
+
+	public static void main(String[] args){
+
+		Scanner sc= new Scanner(System.in);
+
+		String s1= sc.nextLine();
+		int n1= sc.nextInt();
+
+		sc.nextLine();
+
+		String s2= sc.nextLine();
+		int n2= sc.nextInt();
+
+		Student S1= new Student(s1,n1);
+		Student S2= new Student(s2,n2);
+
+		S1.print();
+		S2.print();
+
+		sc.close();
     
+
+	}
 }
